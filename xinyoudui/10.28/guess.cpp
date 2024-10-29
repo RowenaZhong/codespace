8b3d117517ea8e2aab62f0db9772ce293f17b98f
RowenaZhong <101788305+PeterZhongCFS@users.noreply.github.com>
2024-10-29 01:06:04 +0000

    80pts guess

--- xinyoudui/10.28/guess.cpp
+++ xinyoudui/10.28/guess.cpp
@@ -38,9 +38,100 @@ inline void writei(T x)
     }
 }
 
+#define lowbit(x) (x) & (-x)
+
+int n, m;
+char str[3005][24];
+
+inline int cnt1(int x)
+{
+    int ans = 0;
+    while (x)
+        x -= lowbit(x), ans++;
+    return ans;
+}
+
+bool check(int x)
+{
+    for (int k = 0; k < (1 << m); k++)
+    {
+        if (cnt1(k) == x)
+        {
+            set<string> a;
+            for (int i = 0; i < n; i++)
+            {
+                string s;
+                for (int j = 0; j < m; j++)
+                {
+                    if (k & (1 << j))
+                    {
+                        s.push_back(str[i][j]);
+                    }
+                }
+                if (a.count(s))
+                    goto kel;
+                a.insert(s);
+            }
+            return true;
+        }
+    kel:;
+    }
+    return false;
+}
+int countit(int x)
+{
+    int ret = 0;
+    for (int k = 0; k < (1 << m); k++)
+    {
+        if (cnt1(k) == x)
+        {
+            set<string> a;
+            for (int i = 0; i < n; i++)
+            {
+                string s;
+                for (int j = 0; j < m; j++)
+                {
+                    if (k & (1 << j))
+                    {
+                        s.push_back(str[i][j]);
+                    }
+                }
+                if (a.count(s))
+                    goto kel;
+                a.insert(s);
+            }
+            ret++;
+        }
+    kel:;
+    }
+    return ret;
+}
 int main()
 {
-    freopen(".in", "r", stdin);
-    freopen(".out", "w", stdout);
+    freopen("guess.in", "r", stdin);
+    freopen("guess.out", "w", stdout);
+    readi(n);
+    readi(m);
+    for (int i = 0; i < n; i++)
+        scanf("%s", str[i]);
+    if (n == 1)
+    {
+        printf("0 1\n");
+        return 0;
+    }
+    int l = 1, r = m;
+    while (l != r)
+    {
+        int mid = (l + r) >> 1;
+        if (check(mid))
+            r = mid;
+        else
+            l = mid + 1;
+    }
+    countit(l);
+    writei(l);
+    putchar(' ');
+    writei(countit(l));
+    putchar('\n');
     return 0;
 }
