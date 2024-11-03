eacf53cd9351e042f0eb5b07a9415da88d7e758e
Rowena <1374682909@qq.com>
2024-11-03 17:23:42 +0800

    Accept P1131

--- 202411/Week10/P1131.cpp
+++ 202411/Week10/P1131.cpp
@@ -21,21 +21,31 @@ public:
 };
 P *e[500007];
 
-long long ans;
-long long dfs(int x, int fa)
+long long ans, dep[500007];
+bool vis[500007];
+int stk_x[500007];
+int stk_cnt[500007];
+int stk_mode[500007];
+int tot = 0;
+void dfs(int x)
 {
-    P *store = nullptr;
-    long long max_depth = 0;
+    // cerr << x << endl;
+    int cnt = 0;
+    vis[x] = true;
     for (P *p = e[x]; p; p = p->next)
     {
-        if (p->to == fa)
+        if (vis[p->to])
             continue;
-        store = new P(p->to, dfs(p->to, x) + p->weight, store);
-        max_depth = max(max_depth, store->weight);
+        dfs(p->to);
+        if (dep[p->to] + p->weight > dep[x])
+        {
+            ans += (dep[p->to] + p->weight - dep[x]) * cnt;
+            dep[x] = dep[p->to] + p->weight;
+        }
+        else
+            ans += dep[x] - (dep[p->to] + p->weight);
+        cnt++;
     }
-    for (P *p = store; p; p = p->next)
-        ans = ans + max_depth - p->weight;
-    return max_depth;
 }
 int n, s, a, b, t;
 int main()
@@ -47,7 +57,7 @@ int main()
         e[a] = new P(b, t, e[a]);
         e[b] = new P(a, t, e[b]);
     }
-    dfs(s, 0);
+    dfs(s);
     cout << ans << endl;
     return 0;
 }
