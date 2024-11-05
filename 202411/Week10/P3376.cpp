2f9e953519dd2625db2180660e8ad96c2db4a839
rowena <1374682909@qq.com>
2024-11-05 11:38:42 +0000

    create P3376

---
+++ 202411/Week10/P3376.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, s, t;
+const int MAXN = 201, MAXM = 5007;
+int head[MAXN], nxt[MAXM << 1], to[MAXM << 1], wei[MAXM << 1], cnt = 1;
+
+int main()
+{
+    return 0;
+}
