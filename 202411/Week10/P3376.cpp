8d636b29f4a042021a3a7bf5e1d215215cddf384
RowenaZhong <2704682909@qq.com>
2024-11-05 21:01:00 +0800

    update P3376

--- 202411/Week10/P3376.cpp
+++ 202411/Week10/P3376.cpp
@@ -2,9 +2,27 @@
 using namespace std;
 int n, m, s, t;
 const int MAXN = 201, MAXM = 5007;
-int head[MAXN], nxt[MAXM << 1], to[MAXM << 1], wei[MAXM << 1], cnt = 1;
+int head[MAXN], nxt[MAXM << 1], to[MAXM << 1],  cnt = 1;
+long long wei[MAXM << 1];
+void add(int x, int y, int w)
+{
+    cnt++, nxt[cnt] = head[x], to[cnt] = y, wei[cnt] = w, head[x] = cnt;
+    cnt++, nxt[cnt] = head[y], to[cnt] = x, wei[cnt] = 0, head[y] = cnt;
+}
 
 int main()
 {
+    scanf("%d %d %d %d", &n, &m, &s, &t);
+    while (m--)
+    {
+        int x, y, w;
+        scanf("%d %d %d", &x, &y, &w);
+        add(x, y, w);
+    }
+    long long flow = 0, ans = 0;
+    while(bfs())
+        while(flow = dfs(s, LONG_LONG_MAX))
+            ans += flow;
+    printf("%d\n", ans);
     return 0;
 }
