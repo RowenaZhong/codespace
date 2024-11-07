f23afd1e3133516f36da10aaeda0b10346764665
Rowena <1374682909@qq.com>
2024-11-07 22:04:33 +0800

    Accept P1402

--- 202411/Week10/P1402.cpp
+++ 202411/Week10/P1402.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXN = 107, INF = INT_MAX, SIZE_N = 5 * MAN,
+const int MAXN = 107, INF = INT_MAX, SIZE_N = 5 * MAXN,
           SIZE_M = 2 * (MAXN * 3 + MAXN * MAXN * 2);
 int n, p, q;
 inline int GetRoomIndex(int idx)
@@ -33,17 +33,20 @@ inline void AddEdge(int u, int v)
 }
 bool bfs()
 {
-    memset(dis, 0x00, sizeof dis);
-    memcpy(now, head, sizeof head);
+    memset(dis, 0x00, sizeof(dis));
+    memcpy(now, head, sizeof(head));
     queue<int> q;
     dis[s] = 1;
     q.push(s);
-    while (q.size()) {
+    while (q.size())
+    {
         const auto u = q.front();
         q.pop();
-        for (auto e = head[u]; e; e = nxt[e]) {
+        for (auto e = head[u]; e; e = nxt[e])
+        {
             const auto v = ver[e];
-            if (cap[e] && !dis[v]) {
+            if (cap[e] && !dis[v])
+            {
                 dis[v] = dis[u] + 1;
                 q.push(v);
                 if (v == t)
@@ -58,15 +61,21 @@ int dinic(int u, int flow)
     if (u == t)
         return flow;
     auto rest = flow, e = now[u];
-    for (; e && rest; e = nxt[e]) {
+    for (; e && rest; e = nxt[e])
+    {
         auto v = ver[e];
-        if (cap[e] && dis[v] == dis[u] + 1) {
+        if (cap[e] && dis[v] == dis[u] + 1)
+        {
             auto subFlow = dinic(v, min(cap[e], rest));
             if (!subFlow)
                 dis[v] = 0;
+            cap[e] -= subFlow;
+            cap[e ^ 1] += subFlow;
+            rest -= subFlow;
         }
     }
     now[u] = e;
+    return flow - rest;
 }
 int main()
 {
@@ -77,16 +86,20 @@ int main()
         AddEdge(s, GetRoomIndex(i));
     for (int i = 1; i <= q; i++)
         AddEdge(GetDishIndex(i), t);
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= p; j++) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= p; j++)
+        {
             int t;
             scanf("%d", &t);
             if (t)
                 AddEdge(GetRoomIndex(j), GetInPersonIndex(i));
         }
     }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= q; j++) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= q; j++)
+        {
             int t;
             scanf("%d", &t);
             if (t)
@@ -97,6 +110,6 @@ int main()
     while (bfs())
         while (flow = dinic(s, INF))
             maxFlow += flow;
-    printf("%d\n", flow);
+    printf("%d\n", maxFlow);
     return 0;
 }
