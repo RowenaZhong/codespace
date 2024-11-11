7d11fa62043f85a6d0210e56d189d66b2760dea4
Rowena <13912340743@163.com>
2024-11-11 10:26:12 +0800

    exchange from pr

--- 202411/daily/11.11/swap.cpp
+++ 202411/daily/11.11/swap.cpp
@@ -1,46 +1,38 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-template <typename T>
-inline void readi(T &x)
-{
-    T f = 1;
-    x = 0;
-    int ch;
-    while (ch = getchar(), !feof(stdin) && !isdigit(ch) && ch != '-')
-        ;
-    if (ch == '-')
-        f = -f, ch = getchar();
-    while (!feof(stdin) && isdigit(ch))
-    {
-        x = (x << 3) + (x << 1) + ch - 0x30;
-        ch = getchar();
-    }
-    x *= f;
-}
-template <typename T>
-inline void writei(T x)
-{
-    if (x == 0)
-    {
-        putchar('0');
-        return;
-    }
-    if (x < 0)
-        x = -x, putchar('-');
-    T p = 1;
-    while (x / p >= 10)
-        p = (p << 3) + (p << 1);
-    while (p)
-    {
-        putchar(x / p + 0x30);
-        x %= p;
-        p /= 10;
-    }
-}
-
-int main()
-{
-    freopen("swap.in", "r", stdin);
-    freopen("swap.out", "w", stdout);
-    return 0;
+#define int unsigned long long
+string s1;
+int m;
+vector<int> pl[10];
+int p10[21];
+signed main(){
+	freopen("swap.in","r",stdin);
+	freopen("swap.out","w",stdout);
+	cin>>s1>>m;
+	int n=s1.size();
+	s1=" "+s1;
+	p10[0]=1;
+	for(int i=1;i<=18;i++) p10[i]=p10[i-1]*10;
+	int sd=0;
+	int now1,now2;
+	for(int i=9;i>=1;i--){
+		for(int j=1;j<=n;j++) if(s1[j]==i+'0') pl[i].push_back(j);
+		for(int j=0;j<pl[i].size();j++){
+			now1=now2=0;
+			for(int k=sd+1;k<=pl[i][j];k++)
+				now1=now1*10+s1[k]-'0';
+			now2=i;
+			for(int k=sd+1;k<pl[i][j];k++) 
+				now2=now2*10+s1[k]-'0';
+			if((now2-now1)*p10[n-pl[i][j]]<m*(pl[i][j]-sd-1)){
+				for(int i=1;i<=n;i++) cout<<s1[i];
+				return 0;
+			}
+			sd++;
+			for(int k=pl[i][j];k>=sd+1;k--) s1[k]=s1[k-1];
+			s1[sd]=i+'0';
+		}
+	}
+	for(int i=1;i<=n;i++) cout<<s1[i];
+	return 0;
 }
