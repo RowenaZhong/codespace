9cdbdd6140215426d8af0d46ca092619cd5bce6d
Rowena <13912340743@163.com>
2024-11-11 10:27:03 +0800

    clang-format

--- 202411/daily/11.11/room.cpp
+++ 202411/daily/11.11/room.cpp
@@ -1,72 +1,92 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int a[1005][1005],vis[1005][1005],tot,n,m;
-vector<pair<int,int> > p;
+int a[1005][1005], vis[1005][1005], tot, n, m;
+vector<pair<int, int>> p;
 char c;
-void dfs(int x,int y){
-	if(x<0||x>n||y<0||y>m||vis[x][y]||!a[x][y]) return;
-	vis[x][y]=tot;
-	dfs(x+1,y);dfs(x,y+1);dfs(x-1,y);dfs(x,y-1);
+void dfs(int x, int y)
+{
+    if (x < 0 || x > n || y < 0 || y > m || vis[x][y] || !a[x][y])
+        return;
+    vis[x][y] = tot;
+    dfs(x + 1, y);
+    dfs(x, y + 1);
+    dfs(x - 1, y);
+    dfs(x, y - 1);
 }
 int fa[1000005];
-int get(int x){
-	if(fa[x]==x) return x;
-	return fa[x]=get(fa[x]);
+int get(int x)
+{
+    if (fa[x] == x)
+        return x;
+    return fa[x] = get(fa[x]);
 }
-int main(){
-	freopen("room.in","r",stdin);
-	freopen("room.out","w",stdout);
-	scanf("%d%d",&n,&m);
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=m;j++){
-			cin>>c;
-			if(c=='.') a[i][j]=1;
-			else p.push_back(make_pair(i,j));
-		}
-	}
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=m;j++){
-			if(!vis[i][j]&&a[i][j]){
-				tot++;
-				dfs(i,j);
-			}
-		}
-	}
-	vector<int> s;
-	int x,y,z,w;
-	int st=vis[1][1],ed=vis[n][m];
-	int ans=0;
-	for(int i=0;i<p.size();i++){
-		for(int j=i+1;j<p.size();j++){
-			for(int k=1;k<=tot;k++) fa[k]=k;
-			s.clear();
-			x=p[i].first,y=p[i].second;
-			if(vis[x+1][y]) s.push_back(vis[x+1][y]);
-			if(vis[x][y+1]) s.push_back(vis[x][y+1]);
-			if(vis[x][y-1]) s.push_back(vis[x][y-1]);
-			if(vis[x-1][y]) s.push_back(vis[x-1][y]);
-			if(s.size()) vis[x][y]=s[0];
-			z=x,w=y;
-			for(int i=1;i<s.size();i++){
-				if(get(s[i])!=get(s[0]))
-					fa[get(s[i])]=get(s[0]);
-			}
-			x=p[j].first,y=p[j].second;
-			s.clear();
-			if(vis[x+1][y]) s.push_back(vis[x+1][y]);
-			if(vis[x][y+1]) s.push_back(vis[x][y+1]);
-			if(vis[x][y-1]) s.push_back(vis[x][y-1]);
-			if(vis[x-1][y]) s.push_back(vis[x-1][y]);
-			for(int i=1;i<s.size();i++){
-				if(get(s[i])!=get(s[0]))
-					fa[get(s[i])]=get(s[0]);
-			}
-			if(get(st)==get(ed)){
-				ans++;
-			}
-			vis[z][w]=0;
-		}
-	}
-	cout<<ans<<endl;
-	return 0;
+int main()
+{
+    freopen("room.in", "r", stdin);
+    freopen("room.out", "w", stdout);
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> c;
+            if (c == '.')
+                a[i][j] = 1;
+            else
+                p.push_back(make_pair(i, j));
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (!vis[i][j] && a[i][j]) {
+                tot++;
+                dfs(i, j);
+            }
+        }
+    }
+    vector<int> s;
+    int x, y, z, w;
+    int st = vis[1][1], ed = vis[n][m];
+    int ans = 0;
+    for (int i = 0; i < p.size(); i++) {
+        for (int j = i + 1; j < p.size(); j++) {
+            for (int k = 1; k <= tot; k++)
+                fa[k] = k;
+            s.clear();
+            x = p[i].first, y = p[i].second;
+            if (vis[x + 1][y])
+                s.push_back(vis[x + 1][y]);
+            if (vis[x][y + 1])
+                s.push_back(vis[x][y + 1]);
+            if (vis[x][y - 1])
+                s.push_back(vis[x][y - 1]);
+            if (vis[x - 1][y])
+                s.push_back(vis[x - 1][y]);
+            if (s.size())
+                vis[x][y] = s[0];
+            z = x, w = y;
+            for (int i = 1; i < s.size(); i++) {
+                if (get(s[i]) != get(s[0]))
+                    fa[get(s[i])] = get(s[0]);
+            }
+            x = p[j].first, y = p[j].second;
+            s.clear();
+            if (vis[x + 1][y])
+                s.push_back(vis[x + 1][y]);
+            if (vis[x][y + 1])
+                s.push_back(vis[x][y + 1]);
+            if (vis[x][y - 1])
+                s.push_back(vis[x][y - 1]);
+            if (vis[x - 1][y])
+                s.push_back(vis[x - 1][y]);
+            for (int i = 1; i < s.size(); i++) {
+                if (get(s[i]) != get(s[0]))
+                    fa[get(s[i])] = get(s[0]);
+            }
+            if (get(st) == get(ed)) {
+                ans++;
+            }
+            vis[z][w] = 0;
+        }
+    }
+    cout << ans << endl;
+    return 0;
 }
--- 202411/daily/11.11/swap.cpp
+++ 202411/daily/11.11/swap.cpp
@@ -1,38 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define int unsigned long long
 string s1;
 int m;
 vector<int> pl[10];
 int p10[21];
-signed main(){
-	freopen("swap.in","r",stdin);
-	freopen("swap.out","w",stdout);
-	cin>>s1>>m;
-	int n=s1.size();
-	s1=" "+s1;
-	p10[0]=1;
-	for(int i=1;i<=18;i++) p10[i]=p10[i-1]*10;
-	int sd=0;
-	int now1,now2;
-	for(int i=9;i>=1;i--){
-		for(int j=1;j<=n;j++) if(s1[j]==i+'0') pl[i].push_back(j);
-		for(int j=0;j<pl[i].size();j++){
-			now1=now2=0;
-			for(int k=sd+1;k<=pl[i][j];k++)
-				now1=now1*10+s1[k]-'0';
-			now2=i;
-			for(int k=sd+1;k<pl[i][j];k++) 
-				now2=now2*10+s1[k]-'0';
-			if((now2-now1)*p10[n-pl[i][j]]<m*(pl[i][j]-sd-1)){
-				for(int i=1;i<=n;i++) cout<<s1[i];
-				return 0;
-			}
-			sd++;
-			for(int k=pl[i][j];k>=sd+1;k--) s1[k]=s1[k-1];
-			s1[sd]=i+'0';
-		}
-	}
-	for(int i=1;i<=n;i++) cout<<s1[i];
-	return 0;
+signed main()
+{
+    freopen("swap.in", "r", stdin);
+    freopen("swap.out", "w", stdout);
+    cin >> s1 >> m;
+    int n = s1.size();
+    s1 = " " + s1;
+    p10[0] = 1;
+    for (int i = 1; i <= 18; i++)
+        p10[i] = p10[i - 1] * 10;
+    int sd = 0;
+    int now1, now2;
+    for (int i = 9; i >= 1; i--) {
+        for (int j = 1; j <= n; j++)
+            if (s1[j] == i + '0')
+                pl[i].push_back(j);
+        for (int j = 0; j < pl[i].size(); j++) {
+            now1 = now2 = 0;
+            for (int k = sd + 1; k <= pl[i][j]; k++)
+                now1 = now1 * 10 + s1[k] - '0';
+            now2 = i;
+            for (int k = sd + 1; k < pl[i][j]; k++)
+                now2 = now2 * 10 + s1[k] - '0';
+            if ((now2 - now1) * p10[n - pl[i][j]] < m * (pl[i][j] - sd - 1)) {
+                for (int i = 1; i <= n; i++)
+                    cout << s1[i];
+                return 0;
+            }
+            sd++;
+            for (int k = pl[i][j]; k >= sd + 1; k--)
+                s1[k] = s1[k - 1];
+            s1[sd] = i + '0';
+        }
+    }
+    for (int i = 1; i <= n; i++)
+        cout << s1[i];
+    return 0;
 }
