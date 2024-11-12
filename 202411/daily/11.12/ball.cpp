806df354364fa79803c3a21852a7df2847232e6b
Rowena <13912340743@163.com>
2024-11-12 08:29:23 +0800

    format ball

--- 202411/daily/11.12/ball.cpp
+++ 202411/daily/11.12/ball.cpp
@@ -10,8 +10,7 @@ inline void readi(T &x)
         ;
     if (ch == '-')
         f = -f, ch = getchar();
-    while (!feof(stdin) && isdigit(ch))
-    {
+    while (!feof(stdin) && isdigit(ch)) {
         x = (x << 3) + (x << 1) + ch - 0x30;
         ch = getchar();
     }
@@ -20,8 +19,7 @@ inline void readi(T &x)
 template <typename T>
 inline void writei(T x)
 {
-    if (x == 0)
-    {
+    if (x == 0) {
         putchar('0');
         return;
     }
@@ -30,8 +28,7 @@ inline void writei(T x)
     T p = 1;
     while (x / p >= 10)
         p = (p << 3) + (p << 1);
-    while (p)
-    {
+    while (p) {
         putchar(x / p + 0x30);
         x %= p;
         p /= 10;
@@ -44,18 +41,21 @@ int main()
     freopen("ball.out", "w", stdout);
     int T;
     readi(T);
-    while (T--)
-    {
+    while (T--) {
         long long color[3];
         readi(color[0]);
         readi(color[1]);
         readi(color[2]);
         sort(color, color + 3);
-        if(color[2]==color[0])putchar('6');
-        else if(color[2]==color[0]+1)
-            if(color[0]+color[1]+color[2]==1)putchar('1');
-            else putchar('2');
-        else putchar('0');
+        if (color[2] == color[0])
+            putchar('6');
+        else if (color[2] == color[0] + 1)
+            if (color[0] + color[1] + color[2] == 1)
+                putchar('1');
+            else
+                putchar('2');
+        else
+            putchar('0');
         putchar('\n');
     }
     return 0;
