696291704e122cd9bf79c23e1546a382b5842d19
Rowena <13912340743@163.com>
2024-11-12 09:33:45 +0800

    format power

--- 202411/daily/11.12/power.cpp
+++ 202411/daily/11.12/power.cpp
@@ -10,8 +10,7 @@ inline void readi(T &x)
         ;
     if (ch == '-')
         f = -f, ch = getchar();
-    while (!feof(stdin) && isdigit(ch))
-    {
+    while (!feof(stdin) && isdigit(ch)) {
         x = (x << 3) + (x << 1) + ch - 0x30;
         ch = getchar();
     }
@@ -20,8 +19,7 @@ inline void readi(T &x)
 template <typename T>
 inline void writei(T x)
 {
-    if (x == 0)
-    {
+    if (x == 0) {
         putchar('0');
         return;
     }
@@ -30,39 +28,43 @@ inline void writei(T x)
     T p = 1;
     while (x / p >= 10)
         p = (p << 3) + (p << 1);
-    while (p)
-    {
+    while (p) {
         putchar(x / p + 0x30);
         x %= p;
         p /= 10;
     }
 }
 typedef unsigned long long ull;
+const int MAXN = 2e5 + 7;
 ull n, k, k1;
-ull ys[200005], cnt[200005], cnt2[200005], tot;
+ull divi[MAXN], tot, cnt[MAXN];
 int main()
 {
     freopen("power.in", "r", stdin);
     freopen("power.out", "w", stdout);
     readi(n), readi(k);
     for (ull i = 2; i * i <= k; i++) {
-        if (k % i)
-            continue;
-        tot++, ys[tot] = i;
-        while (k % i == 0)
-            cnt[tot]++, k /= i;
+        if (k % i == 0) {
+            divi[++tot] = i;
+            while (k % i == 0) {
+                cnt[tot]++;
+                k /= i;
+            }
+        }
     }
-    if (k != 1)
-        tot++, ys[tot] = k, cnt[tot] = 1;
+    if (k != 1) {
+        divi[++tot] = k;
+        cnt[tot] = 1;
+    }
     ull ans = LONG_LONG_MAX;
-    ull now;
     for (ull i = 1; i <= tot; i++) {
-        for (now = ys[i]; now <= n; now *= ys[i]) {
-            cnt2[i] += n / now;
-            if (now * ys[i] / ys[i] != now)
+        ull sum = 0;
+        for (ull j = divi[i]; j <= n; j *= divi[i]) {
+            sum += n / j;
+            if (j * divi[i] / divi[i] != j)
                 break;
         }
-        ans = min(ans, cnt2[i] / cnt[i]);
+        ans = min(ans, sum / cnt[i]);
     }
     writei(ans);
     putchar('\n');
