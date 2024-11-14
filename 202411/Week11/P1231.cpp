b37834e4f0e7d18a7e206b51a9cb6db4b2af576d
rowena <1374682909@qq.com>
2024-11-14 03:07:13 +0000

    10pts P1231

---
+++ 202411/Week11/P1231.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 3e4 + 7, MAXM = 1.2e5 + 7, MAX_SUB_N = 1e4;
+int head[MAXN], now[MAXN], dis[MAXN];
+int ver[MAXM], nxt[MAXM], cap[MAXM];
+int tot = 1, s = 3 * MAX_SUB_N + 1, t = 3 * MAX_SUB_N + 2;
+inline int GetBookIdx(int idx)
+{
+    return idx;
+}
+inline int GetTestIdx(int idx)
+{
+    return MAX_SUB_N + idx;
+}
+inline int GetAnsIdx(int idx)
+{
+    return MAX_SUB_N * 2 + idx;
+}
+inline void AddSingleEdge(int u, int v, int w)
+{
+    tot++, nxt[tot] = head[u], head[u] = tot, ver[tot] = v, cap[tot] = w;
+}
+inline void AddEdge(int u, int v, int w)
+{
+    AddSingleEdge(u, v, w);
+    AddSingleEdge(v, u, 0);
+}
+bool bfs()
+{
+    memcpy(now, head, sizeof(head));
+    memset(dis, 0x00, sizeof(dis));
+    queue<int> que;
+    que.push(s), dis[s] = 1;
+    while (que.size())
+    {
+        auto u = que.front();
+        que.pop();
+        for (auto e = head[u]; e; e = nxt[e])
+        {
+            auto v = ver[e];
+            if (!dis[v] && cap[e])
+            {
+                dis[v] = dis[u] + 1;
+                if (v == t)
+                    return true;
+                que.push(v);
+            }
+        }
+    }
+    return false;
+}
+int dinic(int u, int flow)
+{
+    if (u == t)
+        return flow;
+    auto rest = flow;
+    auto e = now[u];
+    for (; e && rest; e = nxt[e])
+    {
+        auto v = ver[e];
+        if (dis[v] == dis[u] + 1 && cap[e])
+        {
+            auto subFlow = dinic(v, min(rest, cap[e]));
+            rest -= subFlow;
+            cap[e] -= subFlow;
+            cap[e ^ 1] += subFlow;
+            if (subFlow == 0)
+                dis[v] = 0;
+        }
+    }
+    now[u] = e;
+    return flow - rest;
+}
+int N1, N2, N3;
+int M1, M2;
+int main()
+{
+    scanf("%d %d %d", &N1, &N2, &N3);
+    for (int i = 1; i <= N2; i++)
+        AddEdge(s, GetTestIdx(i), 1);
+    for (int i = 1; i <= N3; i++)
+        AddEdge(GetAnsIdx(i), t, 1);
+    scanf("%d", &M1);
+    for (int i = 1; i <= M1; i++)
+    {
+        int b, t;
+        scanf("%d %d", &b, &t);
+        AddEdge(GetTestIdx(t), GetBookIdx(b), 1);
+    }
+    scanf("%d", &M2);
+    for (int i = 1; i <= M2; i++)
+    {
+        int b, a;
+        scanf("%d %d", &b, &a);
+        AddEdge(GetBookIdx(b), GetAnsIdx(a), 1);
+    }
+    int maxFlow = 0, flow;
+    while (bfs())
+        while (flow = dinic(s, INT_MAX))
+            maxFlow += flow;
+    printf("%d\n", maxFlow);
+    return 0;
+}
