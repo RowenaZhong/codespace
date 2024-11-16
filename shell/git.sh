db61d51feaa0e7e3a8ea2b8af05685a1f35f4d7b
PeterZhongSCZ <1374682909@qq.com>
2024-11-16 23:34:48 +0800

    flush git

--- shell/git.sh
+++ shell/git.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+ssh -T git@gitee.com
+ssh -T git@github.com
+git fetch gitee
+git fetch github
 git config --global user.name "rowena"
 git config --global user.email "1374682909@qq.com"
 git config --unset commit.gpgsign
