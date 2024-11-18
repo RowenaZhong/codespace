0a358ae5b1c8585f5d4661741e74fc5c640624be
rowena <1374682909@qq.com>
2024-11-18 12:54:50 +0000

    20pts P2575

---
+++ 202411/Week11/P2575.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+int T,n,m,a[25],sg;
+bool selected[21];
+// int cnt[21];
+
+int main() {
+    cin>>T;
+    while(T--)
+    {
+        cin>>n;
+        sg=0;
+        while(n--)
+        {
+            cin>>m;
+            for(int i=0;i<=20;i++)selected[i]=0;
+            int idx=21-m;
+            while(m--)
+            {
+                int x;
+                cin>>x;
+                selected[x]=true;
+            }
+            int cont=0;
+            for(int i=20;i>=0;i--)
+            {
+                if(selected[i])cont++;
+                else
+                {
+                    if((--idx)&1)sg^=cont;
+                    // cnt[++idx]=cont;
+                    cont=0;
+                }
+            }
+        }
+        puts(sg?"YES":"NO");
+    }
+    return 0;
+}
