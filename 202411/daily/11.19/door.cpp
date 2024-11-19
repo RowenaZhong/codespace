91f265ca685cead0939a13a39be9cb401a39d09f
rowena <1374682909@qq.com>
2024-11-19 11:09:17 +0800

    format door

--- 202411/daily/11.19/door.cpp
+++ 202411/daily/11.19/door.cpp
@@ -1,58 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-namespace IO{
-    template <typename T>
-    inline void readi(T &x)
-    {
-        T f = 1;
-        x = 0;
-        int ch;
-        while (ch = getchar(), !feof(stdin) && !isdigit(ch) && ch != '-')
-            ;
-        if (ch == '-')
-            f = -f, ch = getchar();
-        while (!feof(stdin) && isdigit(ch)) {
-            x = (x << 3) + (x << 1) + ch - 0x30;
-            ch = getchar();
-        }
-        x *= f;
+namespace IO
+{
+template <typename T>
+inline void readi(T &x)
+{
+    T f = 1;
+    x = 0;
+    int ch;
+    while (ch = getchar(), !feof(stdin) && !isdigit(ch) && ch != '-')
+        ;
+    if (ch == '-')
+        f = -f, ch = getchar();
+    while (!feof(stdin) && isdigit(ch)) {
+        x = (x << 3) + (x << 1) + ch - 0x30;
+        ch = getchar();
     }
-    inline void readl() {}
-    template <typename T, typename... Args>
-    inline void readl(T &x, Args &...args)
-    {
-        readi(x);
-        readl(args...);
+    x *= f;
+}
+inline void readl() {}
+template <typename T, typename... Args>
+inline void readl(T &x, Args &...args)
+{
+    readi(x);
+    readl(args...);
+}
+template <typename T>
+inline void writei(T x)
+{
+    if (x == 0) {
+        putchar('0');
+        return;
     }
-    template <typename T>
-    inline void writei(T x)
-    {
-        if (x == 0) {
-            putchar('0');
-            return;
-        }
-        if (x < 0)
-            x = -x, putchar('-');
-        T p = 1;
-        while (x / p >= 10)
-            p = (p << 3) + (p << 1);
-        while (p) {
-            putchar(x / p + 0x30);
-            x %= p;
-            p /= 10;
-        }
-    }
-    inline void writel() {}
-    template <typename T, typename... Args>
-    inline void writel(T x, Args... args)
-    {
-        writei(x);
-        putchar(" \n"[sizeof...(args) == 0]);
-        writel(args...);
+    if (x < 0)
+        x = -x, putchar('-');
+    T p = 1;
+    while (x / p >= 10)
+        p = (p << 3) + (p << 1);
+    while (p) {
+        putchar(x / p + 0x30);
+        x %= p;
+        p /= 10;
     }
 }
+inline void writel() {}
+template <typename T, typename... Args>
+inline void writel(T x, Args... args)
+{
+    writei(x);
+    putchar(" \n"[sizeof...(args) == 0]);
+    writel(args...);
+}
+} // namespace IO
 
-const int MAXN=2e6+7;
+const int MAXN = 2e6 + 7;
 bool doorOpenedHistory[MAXN];
 bool doorOpened[MAXN];
 bool buttonPressed[MAXN];
@@ -61,25 +62,32 @@ int buttonPosition[MAXN];
 int n, cntDoor, cntButton;
 void dfs()
 {
-    for(int i=1;i<=cntButton;i++)
-    {
-        if(buttonPressed[i]==false)
-        {
-            buttonPressed[i]=true;
-            int l=lower_bound(doorPosition+1,doorPosition+cntDoor+1,buttonPosition[i])-doorPosition-1;
-            auto r=l+1;
+    for (int i = 1; i <= cntButton; i++) {
+        if (buttonPressed[i] == false) {
+            buttonPressed[i] = true;
+            int l = lower_bound(doorPosition + 1, doorPosition + cntDoor + 1,
+                                buttonPosition[i]) -
+                    doorPosition - 1;
+            auto r = l + 1;
             int x;
-            while(l&&doorOpened[l])l--;
-            while(r<=cntDoor&&doorOpened[r])r++;
-            if(doorOpened[l]&&doorOpened[r])return;
-            if(doorOpened[l])x=r;
-            else if(doorOpened[r])x=l;
-            else if(doorPosition[l]+doorPosition[r]>=buttonPosition[i])x=l;
-            else x=r;
-            doorOpened[x]=doorOpenedHistory[x]=true;
+            while (l && doorOpened[l])
+                l--;
+            while (r <= cntDoor && doorOpened[r])
+                r++;
+            if (doorOpened[l] && doorOpened[r])
+                return;
+            if (doorOpened[l])
+                x = r;
+            else if (doorOpened[r])
+                x = l;
+            else if (doorPosition[l] + doorPosition[r] >= buttonPosition[i])
+                x = l;
+            else
+                x = r;
+            doorOpened[x] = doorOpenedHistory[x] = true;
             dfs();
-            doorOpened[x]=false;
-            buttonPressed[i]=false;
+            doorOpened[x] = false;
+            buttonPressed[i] = false;
         }
     }
 }
@@ -88,15 +96,16 @@ int main()
     freopen("door.in", "r", stdin);
     freopen("door.out", "w", stdout);
     IO::readi(n);
-    for(int i=1;i<=n;i++)
-    {
-        int type,pos;
-        IO::readl(type,pos);
-        if(type==1)doorPosition[++cntDoor]=pos;
-        else buttonPosition[++cntButton]=pos;
+    for (int i = 1; i <= n; i++) {
+        int type, pos;
+        IO::readl(type, pos);
+        if (type == 1)
+            doorPosition[++cntDoor] = pos;
+        else
+            buttonPosition[++cntButton] = pos;
     }
     dfs();
-    for(int i=1;i<=cntDoor;i++)
-        IO::writei((int)doorOpenedHistory[i]);
+    for (int i = 1; i <= cntDoor; i++)
+        IO::writei((int) doorOpenedHistory[i]);
     return 0;
 }
