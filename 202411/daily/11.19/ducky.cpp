2ce3a2631ef03b69a921192d37e69c14768a564f
rowena <1374682909@qq.com>
2024-11-19 01:48:57 +0000

    gdb ducky

---
+++ 202411/daily/11.19/ducky.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+namespace IO{
+    template <typename T>
+    inline void readi(T &x)
+    {
+        T f = 1;
+        x = 0;
+        int ch;
+        while (ch = getchar(), !feof(stdin) && !isdigit(ch) && ch != '-')
+            ;
+        if (ch == '-')
+            f = -f, ch = getchar();
+        while (!feof(stdin) && isdigit(ch)) {
+            x = (x << 3) + (x << 1) + ch - 0x30;
+            ch = getchar();
+        }
+        x *= f;
+    }
+    inline void readl() {}
+    template <typename T, typename... Args>
+    inline void readl(T &x, Args &...args)
+    {
+        readi(x);
+        readl(args...);
+    }
+    template <typename T>
+    inline void writei(T x)
+    {
+        if (x == 0) {
+            putchar('0');
+            return;
+        }
+        if (x < 0)
+            x = -x, putchar('-');
+        T p = 1;
+        while (x / p >= 10)
+            p = (p << 3) + (p << 1);
+        while (p) {
+            putchar(x / p + 0x30);
+            x %= p;
+            p /= 10;
+        }
+    }
+    inline void writel() {}
+    template <typename T, typename... Args>
+    inline void writel(T x, Args... args)
+    {
+        writei(x);
+        putchar(" \n"[sizeof...(args) == 0]);
+        writel(args...);
+    }
+}
+int n,d,L,mod;
+int dfs(int len,int place,int restLen,int restN)
+{
+    if(restN==0&&restLen==0)return 1;
+    if(restN==0||restLen==0)return 0;
+    int ans=0;
+    for(int i=0;i<=d*place;i++)
+    {
+        if(i*len>restLen||i>restN)return 0;
+        ans=(ans+dfs(len+1,place*d-i,restLen-i*len,restN-i))%mod;
+    }
+    return ans;
+}
+int main()
+{
+    freopen("ducky.in", "r", stdin);
+    freopen("ducky.out", "w", stdout);
+    IO::readl(n, d, L, mod);
+    for(int i=1;i<=L;i++)
+    {
+        if(i<n){puts("0");continue;}
+        IO::writel(dfs(1,1,L,n));
+    }
+    return 0;
+}
