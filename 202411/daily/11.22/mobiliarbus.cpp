b20574cc3d377d33e6c053aeb7673feebea4894a
rowena <1374682909@qq.com>
2024-11-22 09:48:19 +0800

    format

--- 202411/daily/11.22/mobiliarbus.cpp
+++ 202411/daily/11.22/mobiliarbus.cpp
@@ -1,60 +1,61 @@
 //molly bless rowena
 #include <bits/stdc++.h>
 using namespace std;
-namespace IO{
-    template <typename T>
-    inline void readi(T &x)
-    {
-        T f = 1;
-        x = 0;
-        int ch;
-        while (ch = getchar(), !feof(stdin) && !isdigit(ch) && ch != '-')
-            ;
-        if (ch == '-')
-            f = -f, ch = getchar();
-        while (!feof(stdin) && isdigit(ch)) {
-            x = (x << 3) + (x << 1) + ch - 0x30;
-            ch = getchar();
-        }
-        x *= f;
+namespace IO
+{
+template <typename T>
+inline void readi(T &x)
+{
+    T f = 1;
+    x = 0;
+    int ch;
+    while (ch = getchar(), !feof(stdin) && !isdigit(ch) && ch != '-')
+        ;
+    if (ch == '-')
+        f = -f, ch = getchar();
+    while (!feof(stdin) && isdigit(ch)) {
+        x = (x << 3) + (x << 1) + ch - 0x30;
+        ch = getchar();
     }
-    inline void readl() {}
-    template <typename T, typename... Args>
-    inline void readl(T &x, Args &...args)
-    {
-        readi(x);
-        readl(args...);
+    x *= f;
+}
+inline void readl() {}
+template <typename T, typename... Args>
+inline void readl(T &x, Args &...args)
+{
+    readi(x);
+    readl(args...);
+}
+template <typename T>
+inline void writei(T x)
+{
+    if (x == 0) {
+        putchar('0');
+        return;
     }
-    template <typename T>
-    inline void writei(T x)
-    {
-        if (x == 0) {
-            putchar('0');
-            return;
-        }
-        if (x < 0)
-            x = -x, putchar('-');
-        T p = 1;
-        while (x / p >= 10)
-            p = (p << 3) + (p << 1);
-        while (p) {
-            putchar(x / p + 0x30);
-            x %= p;
-            p /= 10;
-        }
-    }
-    inline void writel() {}
-    template <typename T, typename... Args>
-    inline void writel(T x, Args... args)
-    {
-        writei(x);
-        putchar(" \n"[sizeof...(args) == 0]);
-        writel(args...);
+    if (x < 0)
+        x = -x, putchar('-');
+    T p = 1;
+    while (x / p >= 10)
+        p = (p << 3) + (p << 1);
+    while (p) {
+        putchar(x / p + 0x30);
+        x %= p;
+        p /= 10;
     }
 }
-const int MAXN=1e6+7;
+inline void writel() {}
+template <typename T, typename... Args>
+inline void writel(T x, Args... args)
+{
+    writei(x);
+    putchar(" \n"[sizeof...(args) == 0]);
+    writel(args...);
+}
+} // namespace IO
+const int MAXN = 1e6 + 7;
 int a[MAXN];
-long long n,k;
+long long n, k;
 // int lazy[MAXN<<2];
 // inline void Spread(int p)
 // {
@@ -85,7 +86,7 @@ long long n,k;
 //     if(lc<=mid)Change(lc,rc,offset,l,mid,p*2);
 //     if(rc>mid)Change(lc,rc,offset,mid+1,r,p*2+1);
 // }
-int cnt2 = 0,l2=1;
+int cnt2 = 0, l2 = 1;
 int pos2[MAXN];
 int cnt[3];
 // class P{
@@ -98,106 +99,95 @@ int cnt[3];
 //     }
 // }
 // P* e[MAXN];
-int cnt0,cnt1;
+int cnt0, cnt1;
 int main()
 {
     freopen("mobiliarbus.in", "r", stdin);
     freopen("mobiliarbus.out", "w", stdout);
-    IO::readl(n,k);
-    for (int i=1;i<=n;i++)
-    {
-        IO::readi(a[i]),cnt[a[i]]++;
+    IO::readl(n, k);
+    for (int i = 1; i <= n; i++) {
+        IO::readi(a[i]), cnt[a[i]]++;
     }
-    if(n<=1000&&k<=1000)
-    {
+    if (n <= 1000 && k <= 1000) {
         int cont1 = 0;
-        for (int i=1;i<=n&&k;i++)
-        {
+        for (int i = 1; i <= n && k; i++) {
             int j;
-            switch(a[i])
-            {
+            switch (a[i]) {
                 case 0:
-                    j=i-1;
-                    while(j&&a[j]==1&&k)
-                    {
-                        swap(a[j],a[j+1]);
+                    j = i - 1;
+                    while (j && a[j] == 1 && k) {
+                        swap(a[j], a[j + 1]);
                         k--;
                         j--;
                     }
                     break;
                 case 1:
-                    j=l2;
-                    for(j;j<=cnt2;j++)
-                    {
-                        if(i-pos2[j]<=k)
-                        {
-                            k-=i-pos2[j];
-                            for(int p=i-1;p>=pos2[j];p--)
-                                swap(a[p],a[p+1]);
-                            for(int p=j;p<=cnt2;p++)
+                    j = l2;
+                    for (j; j <= cnt2; j++) {
+                        if (i - pos2[j] <= k) {
+                            k -= i - pos2[j];
+                            for (int p = i - 1; p >= pos2[j]; p--)
+                                swap(a[p], a[p + 1]);
+                            for (int p = j; p <= cnt2; p++)
                                 pos2[p]++;
                             break;
                         }
                     }
-                    l2=j;
+                    l2 = j;
                     break;
                 case 2:
-                    pos2[++cnt2]=i;
+                    pos2[++cnt2] = i;
                     break;
             }
         }
-        for(int i=1;i<=n;i++)
-        {
+        for (int i = 1; i <= n; i++) {
             IO::writei(a[i]);
-            putchar(" \n"[i==n]);
+            putchar(" \n"[i == n]);
         }
         return 0;
     }
-    if(n*n<=k)
-    {
-        int cnt0=0;
-        int first2=0;
-        for(int i=1;i<=n;i++)
-        {
-            if(a[i]==2){first2=i;break;}
-            if(a[i]==0)cnt0++;
+    if (n * n <= k) {
+        int cnt0 = 0;
+        int first2 = 0;
+        for (int i = 1; i <= n; i++) {
+            if (a[i] == 2) {
+                first2 = i;
+                break;
+            }
+            if (a[i] == 0)
+                cnt0++;
         }
-        for(int i=1;i<=cnt0;i++)
-            putchar('0'),putchar(' ');
-        for(int i=1;i<=cnt[1];i++)
-            putchar('1'),putchar(' ');
-        for(int i=first2;i<=n;i++)
-        {
-            if(a[i]!=1)putchar(a[i]+0x30),putchar(" \n"[i==n]);
+        for (int i = 1; i <= cnt0; i++)
+            putchar('0'), putchar(' ');
+        for (int i = 1; i <= cnt[1]; i++)
+            putchar('1'), putchar(' ');
+        for (int i = first2; i <= n; i++) {
+            if (a[i] != 1)
+                putchar(a[i] + 0x30), putchar(" \n"[i == n]);
         }
         putchar('\n');
         return 0;
     }
-    if(cnt[2]==0)
-    {
-        for(int i=1;i<=n;i++)
-        {
-            if(a[i]==0)
-            {
-                int req=i-(cnt0+1);
-                if(req<=k)
-                {
-                    k-=req;
+    if (cnt[2] == 0) {
+        for (int i = 1; i <= n; i++) {
+            if (a[i] == 0) {
+                int req = i - (cnt0 + 1);
+                if (req <= k) {
+                    k -= req;
                     cnt0++;
-                }
-                else
-                {
-                    int xi=i-k;
-                    for(int j=1;j<=cnt0;j++)putchar('0'),putchar(' ');
-                    for(int j=cnt0+1;j<=i;j++)
-                    {
-                        if(j==xi)putchar('0');
-                        else putchar('1');
+                } else {
+                    int xi = i - k;
+                    for (int j = 1; j <= cnt0; j++)
+                        putchar('0'), putchar(' ');
+                    for (int j = cnt0 + 1; j <= i; j++) {
+                        if (j == xi)
+                            putchar('0');
+                        else
+                            putchar('1');
                         putchar(' ');
                     }
-                    for(int j=i+1;j<=n;j++)
-                    {
-                        putchar(a[i]+0x30),putchar(" \n"[i==n]);
+                    for (int j = i + 1; j <= n; j++) {
+                        putchar(a[i] + 0x30), putchar(" \n"[i == n]);
                     }
                     break;
                 }
