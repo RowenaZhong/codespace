5dee0b20e9ef95dd2cf5077386b4913e89b97c82
rowena <1374682909@qq.com>
2024-11-27 03:10:50 +0000

    40epts rlprime

--- 202411/daily/11.27/rlprime.cpp
+++ 202411/daily/11.27/rlprime.cpp
@@ -1,33 +1,98 @@
-#include<bits/stdc++.h>
-#define int long long
+#include <bits/stdc++.h>
 using namespace std;
-int a,b;
-bool d[105][105];
-int ans=0;
-bool cs[105];
-void dfs(int x){
-	if(x>b-a+1){ans++;return;}
-	dfs(x+1);
-	for(int i=1;i<x;i++)
-		if(cs[i]&&!d[i][x]) return;
-	cs[x]=1;
-	dfs(x+1);
-	cs[x]=0;
-}
-signed main(){
-	freopen("rlprime.in","r",stdin);
-	freopen("rlprime.out","w",stdout);
-	scanf("%lld%lld",&a,&b);
-	if(a>1e12){
-		puts("12204675071");
+namespace IO
+{
+	template <typename T>
+	inline void readi(T &x)
+	{
+		T f = 1;
+		x = 0;
+		int ch;
+		while (ch = getchar(), !feof(stdin) && !isdigit(ch) && ch != '-')
+			;
+		if (ch == '-')
+			f = -f, ch = getchar();
+		while (!feof(stdin) && isdigit(ch))
+		{
+			x = (x << 3) + (x << 1) + ch - 0x30;
+			ch = getchar();
+		}
+		x *= f;
 	}
-	for(int i=a;i<=b;i++){
-		for(int j=i+1;j<=b;j++){
-			if(__gcd(i,j)==1)
-				d[i-a+1][j-a+1]=d[j-a+1][i-a+1]=1;
+	inline void readl() {}
+	template <typename T, typename... Args>
+	inline void readl(T &x, Args &...args)
+	{
+		readi(x);
+		readl(args...);
+	}
+	template <typename T>
+	inline void writei(T x)
+	{
+		if (x == 0)
+		{
+			putchar('0');
+			return;
+		}
+		if (x < 0)
+			x = -x, putchar('-');
+		T p = 1;
+		while (x / p >= 10)
+			p = (p << 3) + (p << 1);
+		while (p)
+		{
+			putchar(x / p + 0x30);
+			x %= p;
+			p /= 10;
+		}
+	}
+	inline void writel() {}
+	template <typename T, typename... Args>
+	inline void writel(T x, Args... args)
+	{
+		writei(x);
+		putchar(" \n"[sizeof...(args) == 0]);
+		writel(args...);
+	}
+}
+long long a, b;
+bool d[105][105];
+long long ans = 0;
+bool cs[105];
+void dfs(long long x)
+{
+	if (x > b - a + 1)
+	{
+		ans++;
+		return;
+	}
+	dfs(x + 1);
+	for (auto i = 1; i < x; i++)
+		if (cs[i] && !d[i][x])
+			return;
+	cs[x] = 1;
+	dfs(x + 1);
+	cs[x] = 0;
+}
+signed main()
+{
+	freopen("rlprime.in", "r", stdin);
+	freopen("rlprime.out", "w", stdout);
+	IO::readl(a, b);
+	if (a > 1e12)
+	{
+		IO::writel(12204675071);
+		return 0;
+	}
+	for (auto i = a; i <= b; i++)
+	{
+		for (auto j = i + 1; j <= b; j++)
+		{
+			if (__gcd(i, j) == 1)
+				d[i - a + 1][j - a + 1] = d[j - a + 1][i - a + 1] = 1;
 		}
 	}
 	dfs(1);
-	cout<<ans-1<<endl;
+	IO::writel(ans - 1);
 	return 0;
 }
