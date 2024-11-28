a69e8faa3651d300fad7199a585e9953322fd9d0
rowena <1374682909@qq.com>
2024-11-28 02:07:16 +0000

    Add D-Test.cpp and fix D.cpp input files

--- 202411/daily/11.28/D-Test.cpp
+++ 202411/daily/11.28/D-Test.cpp
@@ -9,21 +9,14 @@ bool cmp(string a, string b)
 const int MOD = 998244353;
 int main()
 {
-    freopen("D.in", "r", stdin);
-    freopen("D.out", "w", stdout);
+    freopen("D-Test.in", "r", stdin);
+    freopen("D-Test.out", "w", stdout);
     getline(cin, str[0]);
     for (int i = 1; i < str[0].size(); i++)
         str[i] = str[i - 1],
         str[i].pop_back();
     sort(str, str + str[0].size(), cmp);
-    long long ans = 0;
     for (int i = 0; i < str[0].size(); i++)
-    {
-        for (int j = 0; j < str[i].size(); j++)
-        {
-            ans = (ans * 10 + str[i][j] - 0x30) % MOD;
-        }
-    }
-    printf("%lld\n", ans);
+        cout << str[i] << endl;
     return 0;
 }
